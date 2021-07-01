c5d988667e26659a82c72e6601596b08d9b049cb
NeoNeTech <quentin.lloveras@gmail.com>
2021-07-01 19:22:37 +0200

    Update scripts_creation_DL.ps1

--- scripts_creation_DL.ps1
+++ scripts_creation_DL.ps1
@@ -1,4 +1,12 @@
-﻿
+#===================================#
+# Exemple :
+#  "Entrez le chemin absolu de l'arbo : E:\"
+# Crée tout les DL correspondant a ce qui a en DESSOUS de E:\ mais ne crée pas de DL pour E:\ 
+#
+# "Entrez le chemin absolu de l'arbo : E:\Entite"
+# Crée tout les DL correspondant a ce qui a en DESSOUS de E:\Entite mais ne crée pas de DL pour Entite
+#===================================#
+
 
 New-ADOrganizationalUnit -Name DL -Path "CHEMIN OU POUR STOCKAGE OU DL ex: ou=entite,dc=domaine,dc=fr"
 
